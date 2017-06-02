3073b6a11f217518424cd9a6762a4eb9df2dada3
iwoz <zameer.b.zubair@gmail.com>
2017-06-02 10:05:49 +0400

    more keyword

--- src/models/headlines/constant.py
+++ src/models/headlines/constant.py
@@ -3,4 +3,4 @@ COLLECTION = 'head_lines'
 rm_list = ['trump', 'Trump',' cow ', 'Cow ', 'COW ', 'cow ',
            ' cow', 'TRUMP', 'അറവുശാല', 'modi', 'Modi ', ' Modi',
            'slaughter', 'Slaughter', 'rape', ' rapi', 'adithyanantha',
-           'Adithyanantha', 'stabbing', 'Terror', 'terror']
+           'Adithyanantha', 'stabbing', 'Terror', 'terror', 'കശാപ്പ് ']
