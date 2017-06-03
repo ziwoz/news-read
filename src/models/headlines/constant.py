a559261ac4c3a473c1f025908e4ff9df9872edeb
zameer <zameer@zameers-MacBook-Pro.local>
2017-06-03 10:22:21 +0400

    more keywords

--- src/models/headlines/constant.py
+++ src/models/headlines/constant.py
@@ -4,4 +4,6 @@ rm_list = ['trump', 'Trump',' cow ', 'Cow ', 'COW ', 'cow ',
            ' cow', 'TRUMP', 'അറവുശാല', 'modi', 'Modi ', ' Modi',
            'slaughter', 'Slaughter', 'rape', ' rapi', 'adithyanantha',
            'Adithyanantha', 'stabbing', 'Terror', 'terror', 'കശാപ്പ് നിരോധനം',
-           'ക്കൊന്നു', 'കശ്മീ', 'പാക്കിസ്ഥാൻ', 'കന്നുകാലി', 'Pakistan', 'ashmir']
+           'ക്കൊന്നു', 'കശ്മീ', 'പാക്കിസ്ഥാൻ', 'കന്നുകാലി', 'Pakistan', 'ashmir',
+           'Rape','Trump', 'ISIS', 'ആർഎസ്എസ്' ,'ബീഫ്', 'Beef', 'beef',
+           'Adityanath', 'ആദിത്യനാഥ്']
